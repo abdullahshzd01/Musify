b58fb22e262da3a02db6fd18e0dbd3816a83f0e4
Abdullah Shahzad <abdullahshzd.69@gmail.com>
2022-09-21 23:22:55 +0500

    Initial commit

---
+++ app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
@@ -0,0 +1,26 @@
+package com.abdullahshahzad.i192012;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(this)
+            }
+        });
+    }
+}
