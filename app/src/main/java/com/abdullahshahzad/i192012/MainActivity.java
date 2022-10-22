c5479903298ce01b4b1952a8910c54b3ca830ea4
Abdullah Shahzad <abdullahshzd.69@gmail.com>
2022-10-23 03:06:01 +0500

    Code update (files updated)

--- app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
+++ app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
@@ -8,19 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
-        button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent intent = new Intent(this)
-            }
-        });
     }
 }
