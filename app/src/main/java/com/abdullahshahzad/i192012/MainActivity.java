2b581a636a03c0d600cacc18894832439fbd9a56
Abdullah Shahzad <abdullahshzd.69@gmail.com>
2022-10-23 00:48:16 +0500

    Code update (files updated)

--- app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
+++ app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
 
         button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
