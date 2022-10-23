3136ffbb6b043d2d79a1629a04d7df47c6d1de55
Abdullah Shahzad <abdullahshzd.69@gmail.com>
2022-10-23 18:27:29 +0500

    Code update (files updated)

--- app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
+++ app/src/main/java/com/abdullahshahzad/i192012/MainActivity.java
@@ -1,5 +1,6 @@
 package com.abdullahshahzad.i192012;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,23 @@ import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    EditText name, email, pass;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +40,61 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 5000);
 
-//        setContentView(R.layout.activity_main);
-//
-//        Button next = (Button) findViewById(R.id.button1);
-//        next.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                setContentView(R.layout.main_page);
-//            }
-//        });
+        mAuth = FirebaseAuth.getInstance();
 
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.mail);
+        pass = findViewById(R.id.pass);
+        signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(
+                                            MainActivity.this,
+                                            "Successfully Created User",
+                                            Toast.LENGTH_LONG
+                                    ).show();
+                                    Toast.makeText(
+                                            MainActivity.this,
+                                             mAuth.getCurrentUser().getUid(),
+                                            Toast.LENGTH_LONG
+                                    ).show();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(
+                                        MainActivity.this,
+                                        "Failed to create User",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null)
+        {
+            Toast.makeText(
+                    MainActivity.this,
+                    user.getUid()+"",
+                    Toast.LENGTH_SHORT
+            );
+        }
     }
 }
